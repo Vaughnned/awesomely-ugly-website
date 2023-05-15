d74abd25cee40182f3c56e36856689b4de178d1b
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-15 16:40:23 -0400

    fixed a few bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,19 @@
-function changeImg() {
-  var img = document.getElementById("trashcan1");
-  img.setAttribute(
-    "src",
-    "https://media.tenor.com/yheo1GGu3FwAAAAM/rick-roll-rick-ashley.gif"
-  );
-  var img2 = document.getElementById("trashcan2");
-  img2.setAttribute(
-    "src",
-    "https://media.tenor.com/yheo1GGu3FwAAAAM/rick-roll-rick-ashley.gif"
-  );
-}
+(function () {
+  "use strict";
 
-// document.querySelector("button").addEventListener("click", getRolled);
+  function changeImg() {
+    var img = document.getElementById("trashcan1");
+    img.setAttribute(
+      "src",
+      "https://media.tenor.com/yheo1GGu3FwAAAAM/rick-roll-rick-ashley.gif"
+    );
+
+    var img2 = document.getElementById("trashcan2");
+    img2.setAttribute(
+      "src",
+      "https://media.tenor.com/yheo1GGu3FwAAAAM/rick-roll-rick-ashley.gif"
+    );
+  }
+
+  document.querySelector("button").addEventListener("click", changeImg);
+})();
