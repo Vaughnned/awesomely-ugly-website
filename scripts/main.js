ce04a554488c795c7d30921453cf28447957a696
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-15 16:18:41 -0400

    uglyness achieved

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+function changeImg() {
+  var img = document.getElementById("trashcan1");
+  img.setAttribute(
+    "src",
+    "https://media.tenor.com/yheo1GGu3FwAAAAM/rick-roll-rick-ashley.gif"
+  );
+  var img2 = document.getElementById("trashcan2");
+  img2.setAttribute(
+    "src",
+    "https://media.tenor.com/yheo1GGu3FwAAAAM/rick-roll-rick-ashley.gif"
+  );
+}
+
+// document.querySelector("button").addEventListener("click", getRolled);
